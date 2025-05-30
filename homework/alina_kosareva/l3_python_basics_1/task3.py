2b6b723f9bbb03444427e38d4bda22fbcfa5c389
AlinaKas24 <alina.kasperovich9@gmail.com>
2025-05-30 16:18:54 +0500

    homework_lesson_3

---
+++ homework/alina_kosareva/l3_python_basics_1/task3.py
@@ -0,0 +1,14 @@
+# Eсли хотим самостоятельно задать значения для x и y
+
+# x=int(input())
+# y=int(input())
+
+# Eсли хотим использовать оперделенные значения для x и y
+
+x = 50
+y = 16
+
+arithmetic_mean = (x + y) / 2
+geometric_mean = (x * y) ** 0.5
+
+print(arithmetic_mean, geometric_mean, sep="\n")
