6d7fd05558f164eaacc38ce248e15f8d8a28193e
AlinaKas24 <alina.kasperovich9@gmail.com>
2025-06-10 13:50:16 +0500

    homework_lesson_4

---
+++ homework/alina_kosareva/l4_python_basics_2/home_task.py
@@ -0,0 +1,33 @@
+my_dict = {
+    "tuple": (1, None, "new", True, 3.14),
+    "list": [
+        1,
+        None,
+        "new",
+        True,
+        3.14,
+    ],
+    "dict": {"one": 1, "two": None, "three": "new", "four": True, "five": 3.14},
+    "set": {1, None, "new", True, 3.14},
+}
+
+#list
+my_dict["list"].append("new_element")
+my_dict["list"].pop(1)
+
+#dict
+my_dict["dict"]["six"]='new_element'
+# del my_dict["dict"]["two"]
+my_dict["dict"].pop("three") 
+
+
+#set
+my_dict["set"].add("new_element")
+# my_dict["set"].pop() # любой произовольный элемент из списка 
+my_dict["set"].remove(3.14) # некий определенный элемент из списка
+
+print(my_dict["tuple"][-1])
+print(my_dict["list"])
+print(my_dict["dict"])
+print(my_dict["set"])
+
