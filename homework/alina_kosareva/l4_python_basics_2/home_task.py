a3129955569afe32b373514b2245bf8b28ccf737
AlinaKas24 <alina.kasperovich9@gmail.com>
2025-06-11 10:11:25 +0500

    homework_lesson_4

--- homework/alina_kosareva/l4_python_basics_2/home_task.py
+++ homework/alina_kosareva/l4_python_basics_2/home_task.py
@@ -26,7 +26,7 @@ my_dict["set"].add("new_element")
 # my_dict["set"].pop()  # любой произовольный элемент из списка
 my_dict["set"].remove(3.14)  # некий определенный элемент из списка
 
-print(my_dict["tuple"][-1])  #выводит на экран последний элемент ‘tuple’
+print(my_dict["tuple"][-1])  # выводит на экран последний элемент ‘tuple’
 print(my_dict["list"])
 print(my_dict["dict"])
 print(my_dict["set"])
