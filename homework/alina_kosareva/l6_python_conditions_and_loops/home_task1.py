988d6a9c990a5db181011cdb08f057937616ee92
AlinaKas24 <alina.kasperovich9@gmail.com>
2025-07-01 17:39:49 +0500

    homework_lesson_6

--- homework/alina_kosareva/l6_python_conditions_and_loops/home_task1.py
+++ homework/alina_kosareva/l6_python_conditions_and_loops/home_task1.py
@@ -1,6 +1,8 @@
 punctuation = ".,!?;:-"
-text="Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
-" Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = (
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
+    " Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+)
 words = text.split()
 new_text = []
 for i in words:
