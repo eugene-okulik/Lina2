550c9cf9d4cab750a22e4cb9c608f78cc346d4b7
AlinaKas24 <alina.kasperovich9@gmail.com>
2025-07-01 17:28:44 +0500

    homework_lesson_6

---
+++ homework/alina_kosareva/l6_python_conditions_and_loops/home_task1.py
@@ -0,0 +1,16 @@
+punctuation = ".,!?;:-"
+text="Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+words = text.split()
+new_text = []
+for i in words:
+    if i[-1] in punctuation:
+        core = i[:-1]
+        punct = i[-1]
+    else:
+        core = i
+        punct = ""
+
+    new_word = core + "ing" + punct
+    new_text.append(new_word)
+
+print(" ".join(new_text))
