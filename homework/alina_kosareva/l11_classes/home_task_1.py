a7d30c8ff57b46d1d154aed66166b405117c1572
AlinaKas24 <alina.kasperovich9@gmail.com>
2026-03-31 11:20:08 +0500

    homework_lesson_11

--- homework/alina_kosareva/l11_classes/home_task_1.py
+++ homework/alina_kosareva/l11_classes/home_task_1.py
@@ -18,6 +18,9 @@ class Book:
         self.isbn = isbn
         self.reserved_flag = reserved_flag
 
+    def reserved(self):
+        self.reserved_flag = True
+
     def description(self):
         if self.reserved_flag:
             description = (
@@ -35,15 +38,13 @@ class Book:
         print(description)
 
 
-book1 = Book("Идиот", "Достоевский", 500, reserved_flag=True)
-book2 = Book(
-    "Мастер и Маргарита", "Булгаков", 480, reserved_flag=False
-)  # можем передавать в открытую
-book3 = Book(
-    "Преступление и наказание", "Достоевский", 430
-)  # можем не передавать значение по дефолту будет false
-book4 = Book("Война и мир", "Толстой", 900, reserved_flag=False)
-book5 = Book("Герой нашего времени", "Лермонтов", 250)
+book1 = Book("Идиот", "Достоевский", 500)
+book2 = Book("Мастер и Маргарита", "Булгаков", 480)
+book3 = Book("Преступление и наказание", "Достоевский", 430)
+book4 = Book("Война и мир", "Толстой", 900)
+book5 = Book("Герой нашего времени", "Лермонтов", 100)
+
+book5.reserved()
 
 book1.description()
 book2.description()
@@ -76,7 +77,7 @@ class SchoolTextbooks(Book):
         self.tasks = tasks
 
     def description(self):
-        if self.tasks:
+        if self.reserved_flag:
             description = (
                 f"Название: {self.book_title}, Автор: {self.author}, "
                 f"страниц: {self.number_pages}, предмет: {self.subject}, "
@@ -92,15 +93,13 @@ class SchoolTextbooks(Book):
             print(description)
 
 
-textbook1 = SchoolTextbooks("Алгебра", "Иванов", 200, "Математика", 9, tasks=True)
-textbook2 = SchoolTextbooks(
-    "История мира", "Петров", 180, "История", 8, tasks=False
-)  # можем передавать в открытую
-textbook3 = SchoolTextbooks(
-    "География", "Иванов", 220, "География", 7
-)  # можем не передавать значение по дефолту будет false
-textbook4 = SchoolTextbooks("Физика", "Сидоров", 210, "Физика", 10, tasks=False)
-textbook5 = SchoolTextbooks("Биология", "Смирнова", 190, "Биология", 6)
+textbook1 = SchoolTextbooks("Алгебра", "Иванов", 200, "Математика", 9, True)
+textbook2 = SchoolTextbooks("История мира", "Петров", 180, "История", 8, True)
+textbook3 = SchoolTextbooks("География", "Иванов", 220, "География", 7, True)
+textbook4 = SchoolTextbooks("Физика", "Сидоров", 210, "Физика", 10, True)
+textbook5 = SchoolTextbooks("Биология", "Смирнова", 190, "Биология", 6, True)
+
+textbook1.reserved()
 
 textbook1.description()
 textbook2.description()
